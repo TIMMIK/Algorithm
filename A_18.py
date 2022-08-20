28f38ad22f5916274fd90348f0b235a7ca6e7715
T I M A <107994980+TIMMIK@users.noreply.github.com>
2022-08-20 16:54:52 +0500

    Update A_18.py

--- A_18.py
+++ A_18.py
@@ -1,45 +1,45 @@
 # Greedy algorithms(code):
 
 # Code --- >
-# buildings = {
-# 'B81':{16,17,18},
-# 'B02':{1,11,4,30},
-# 'B03':{17,18,27,30},
-# 'B84':{1,7,17,19,30},
-# 'B05':{7,11,15,17,20},
-# 'B06':{11,6,7},
-# 'B07':{30,23},
-# 'B08':{10,14,18,25,29},
-# 'B09':{16,17,18,19},
-# 'B10':{10,19,13},
-# 'B11':{1,3,6,13,16},
-# 'B12':{8,12},
-# 'B13':{10,5,15},
-# 'B14':{5,8,17,23,24},
-# 'B15':{2,9,21,22,26,28},
-# }
-# regions = {
-#   1,2,3,4,5,6,7,8,9,10,
-#   11,12,13,14,15,16,17,18,19,20,
-#   21,22,23,24,25,26,27,28,29,30,
-# }
+buildings = {
+'B81':{16,17,18},
+'B02':{1,11,4,30},
+'B03':{17,18,27,30},
+'B84':{1,7,17,19,30},
+'B05':{7,11,15,17,20},
+'B06':{11,6,7},
+'B07':{30,23},
+'B08':{10,14,18,25,29},
+'B09':{16,17,18,19},
+'B10':{10,19,13},
+'B11':{1,3,6,13,16},
+'B12':{8,12},
+'B13':{10,5,15},
+'B14':{5,8,17,23,24},
+'B15':{2,9,21,22,26,28},
+}
+regions = {
+  1,2,3,4,5,6,7,8,9,10,
+  11,12,13,14,15,16,17,18,19,20,
+  21,22,23,24,25,26,27,28,29,30,
+}
 
-# final_buildings = set()
-# while regions:
-#     best_building = None
-#     covered_areas = set()
-#     for building, building_scope in buildings.items():
-#         coverage = regions & building_scope
-#         print(f"{building}:{coverage}")
-#         if len(coverage) > len(covered_areas):
-#             best_building = building
-#             covered_areas = coverage
-#     regions -= covered_areas
-#     final_buildings.add(best_building)
-#     print(f"Selected building: {best_building}")
-#     print(f"Remaining areas: {regions}")
-#     print(f"Selected recent buildings: {final_buildings}")
-#     input()
+final_buildings = set()
+while regions:
+    best_building = None
+    covered_areas = set()
+    for building, building_scope in buildings.items():
+        coverage = regions & building_scope
+        print(f"{building}:{coverage}")
+        if len(coverage) > len(covered_areas):
+            best_building = building
+            covered_areas = coverage
+    regions -= covered_areas
+    final_buildings.add(best_building)
+    print(f"Selected building: {best_building}")
+    print(f"Remaining areas: {regions}")
+    print(f"Selected recent buildings: {final_buildings}")
+    input()
 # Traveling Salesperson(en):
 # The travelling salesperson problem (TSP) is a classic optimization problem where the goal is to determine the shortest tour of a collection of n “cities” (i.e. nodes), starting and ending in the same city and visiting all of the other cities exactly once.
 
